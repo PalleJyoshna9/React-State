e58a86351fc45eca4101394b86616bbb4ed8dd30
PalleJyoshna9 <pallejyoshna130@gmail.com>
2025-12-29 18:49:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>React State Example</h1>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default App;
